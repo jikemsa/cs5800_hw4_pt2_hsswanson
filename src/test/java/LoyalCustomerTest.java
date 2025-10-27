c9b71be082de2aa58f85c689f1a53986acd55e85
Hunter <jikemsa@gmail.com>
2025-10-27 16:29:56 -0700

    LoyalCustomerTest completed.

--- src/test/java/LoyalCustomerTest.java
+++ src/test/java/LoyalCustomerTest.java
@@ -1,16 +1,25 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class LoyalCustomerTest {
 
-    private CustomerOrder newOrder = new CustomerOrder();
+    private final CustomerOrder order = new CustomerOrder(new ArrayList<FoodItem>(Arrays.asList(new HotDog(), new Hamburger())));
+    private final CustomerOrder loyalOrder = new LoyalCustomer(order);
+
+
 
     @Test
     void getCost() {
+        assertEquals(.9*order.getCost(),loyalOrder.getCost());
     }
 
     @Test
     void getItems() {
+        assertEquals(order.getItems(),loyalOrder.getItems());
     }
 }
